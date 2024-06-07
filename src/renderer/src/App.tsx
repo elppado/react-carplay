75b3dce4ccfaad0167f8413c130be18a881aa821
doyoc <elppado@icloud.com>
2024-06-07 18:29:01 +0900

    edit

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -6,14 +6,14 @@ import Carplay from './components/Carplay'
 import { useCarplayStore } from './store/store'
 
 
-const Settings = lazy(() => import('./components/Settings'))
-const Carplay = lazy(() => import('./components/Carplay'))
+
 
 function App() {
   const [receivingVideo, setReceivingVideo] = useState(false)
   const [commandCounter, setCommandCounter] = useState(0)
   const [keyCommand, setKeyCommand] = useState('')
   const settings = useCarplayStore((state) => state.settings)
+  // const locationpath = useLocation()
 
   useEffect(() => {
     document.addEventListener('keydown', onKeyDown)
@@ -45,17 +45,15 @@ function App() {
     <Router>
       <div className="full">
         <div style={{ textAlign: 'center' }}>
-          <Suspense fallback={<div>Loading...</div>}>
-            {settings ? (
-              <Carplay
-                receivingVideo={receivingVideo}
-                setReceivingVideo={setReceivingVideo}
-                settings={settings}
-                command={keyCommand}
-                commandCounter={commandCounter}
-              />
-            ) : null}
-          </Suspense>
+          {settings ? (
+            <Carplay
+              receivingVideo={receivingVideo}
+              setReceivingVideo={setReceivingVideo}
+              settings={settings}
+              command={keyCommand}
+              commandCounter={commandCounter}
+            />
+          ) : null}
         </div>
       </div>
     </Router>
@@ -63,58 +61,3 @@ function App() {
 }
 
 export default App
-
-
-// function App() {
-//   const [receivingVideo, setReceivingVideo] = useState(false)
-//   const [commandCounter, setCommandCounter] = useState(0)
-//   const [keyCommand, setKeyCommand] = useState('')
-//   const settings = useCarplayStore((state) => state.settings)
-//   // const locationpath = useLocation()
-
-//   useEffect(() => {
-//     document.addEventListener('keydown', onKeyDown)
-
-//     return () => document.removeEventListener('keydown', onKeyDown)
-//   }, [settings])
-
-//   const onKeyDown = (event: KeyboardEvent): void => {
-//     console.log(event.code)
-//     if (Object.values(settings!.bindings).includes(event.code)) {
-//       const action = Object.keys(settings!.bindings).find(
-//         (key) => settings!.bindings[key] === event.code
-//       )
-//       if (action !== undefined) {
-//         setKeyCommand(action)
-//         setCommandCounter((prev) => prev + 1)
-//         if (action === 'selectDown') {
-//           console.log('select down')
-//           setTimeout(() => {
-//             setKeyCommand('selectUp')
-//             setCommandCounter((prev) => prev + 1)
-//           }, 200)
-//         }
-//       }
-//     }
-//   }
-
-//   return (
-//     <Router>
-//       <div className="full">
-//         <div style={{ textAlign: 'center' }}>
-//           {settings ? (
-//             <Carplay
-//               receivingVideo={receivingVideo}
-//               setReceivingVideo={setReceivingVideo}
-//               settings={settings}
-//               command={keyCommand}
-//               commandCounter={commandCounter}
-//             />
-//           ) : null}
-//         </div>
-//       </div>
-//     </Router>
-//   )
-// }
-
-// export default App
